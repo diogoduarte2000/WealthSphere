4cd6304f5c26aacbd79911dd2d439c852b973f58
Diogo Duarte <diogo.silvanoduarte@gmail.com>
2026-05-10 13:14:51 +0100

    version 5

---
+++ frontend/src/app/pages/perfil/perfil.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  templateUrl: './perfil.html',
+  styleUrl: './perfil.css'
+})
+export class Perfil {}
---
+++ frontend/src/app/pages/taxas/taxas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taxas',
+  standalone: true,
+  templateUrl: './taxas.html',
+  styleUrl: './taxas.css'
+})
+export class Taxas {}
